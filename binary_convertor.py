f3b8835c0618e0a0ce34fcf81a3fe2d8e6e7c237
Jaineel Petiwale <jaineel2001@gmail.com>
2021-02-26 21:18:04 +0530

    binary convertor

--- binary_convertor.py
+++ binary_convertor.py
@@ -1,20 +1,27 @@
 import endecrypt
 
-print("What do you want to do ?")
-print("1. Convert Binary to Character")
-print("2. Convert Character to Binary")
-option=input("Please select your option: ")
+def start():
+    print("What do you want to do ?")
+    print("1. Convert Binary to Character")
+    print("2. Convert Character to Binary")
+    option=input("Please select your option: ")
 
-if option not in ("1","2"):
-    print("Invalid Option")
-else:
-    if option=="1":
-        x=input("Enter binary you want to convert: ")
-        print("character conversion is :")
-        endecrypt.decode(x,'binary')
+    if option not in ("1","2","back"):
+        print("Invalid Option")
+        start()
+    else:
+        if option=="1":
+            x=input("Enter binary you want to convert: ")
+            print("character conversion is :")
+            endecrypt.decode(x,'binary')
 
-    elif option=="2":
-        x=input("Enter character you want to convert: ")
-        print("Binary conversion is :")
+        elif option=="2":
+            x=input("Enter character you want to convert: ")
+            print("Binary conversion is :")
+            endecrypt.encode(x,'binary')
 
-        endecrypt.encode(x,'binary')
+        elif option=="back":
+            start()
+
+start()
+#end
